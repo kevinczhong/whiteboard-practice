5b6ab4c56faec1fa39ebaa0a906c38f8084f538a
Kevin Zhong <kvnzhong@gmail.com>
2022-08-01 16:59:34 -0700

    Modified largestProduct solution; O now = to 0(n^2)

--- largestProduct.py
+++ largestProduct.py
@@ -1,13 +1,10 @@
 def largestProduct(nums):
-    prod = []
+    champ = (nums[0] * nums[1])
     for i in range(len(nums)):
         j = i + 1
         for j in range(j, len(nums) - 1):
-            prod.append(nums[i] * nums[j])
-    champ = prod[0]
-    for k in prod:
-        if k > champ:
-            champ = k
+            if nums[i] * nums[j] > champ:
+                champ = nums[i] * nums[j]
     return champ
 
 
